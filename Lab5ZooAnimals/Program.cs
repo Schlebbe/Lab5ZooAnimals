5257aac3fb5caf0245ec9c9390e276b745af07bb
sebastian@temtron.se <sebastian@temtron.se>
2025-09-26 12:00:43 +0200

    Add project files.

---
+++ Lab5ZooAnimals/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab5ZooAnimals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
