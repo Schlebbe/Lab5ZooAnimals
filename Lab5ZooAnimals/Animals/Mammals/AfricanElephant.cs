ee0e6df0390bb5b28f510dc24972704774a13d5f
sebastian@temtron.se <sebastian@temtron.se>
2025-09-26 13:22:29 +0200

    Added FlapEars to african elephants

--- Lab5ZooAnimals/Animals/Mammals/AfricanElephant.cs
+++ Lab5ZooAnimals/Animals/Mammals/AfricanElephant.cs
@@ -4,10 +4,18 @@ namespace Lab5ZooAnimals.Animals.Mammals
 {
     internal class AfricanElephant : Elephant
     {
+        public int EarSize { get; set; }
+
         public AfricanElephant(string name, int age, int weight, bool isTame) : base(name, age, weight, isTame)
         {
             Species = "African Elephant";
             TrunkLength = 70;
+            EarSize = 20;
+        }
+
+        public void FlapEars()
+        {
+            Console.WriteLine($"{Name} flaps its large ears ({EarSize} cm) to cool down!");
         }
 
         public override void MakeSound()
